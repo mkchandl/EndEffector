2178a7aaa61cda7ba50505369be918c4f3affbf1
mkchandl <mkchandl@calpoly.edu>
2021-11-20 19:31:45 -0800

    flushing out notes on how code will be added and added motor object, some function variables. Thinking about putting the gains as static variables that exist outside of the task funct

--- ConversionTask/ConversionTask.cpp
+++ ConversionTask/ConversionTask.cpp
@@ -12,27 +12,47 @@
     #include <STM32FreeRTOS.h>
 #endif
 #include "ConversionTask.h"
+#include "MotorDriver.h"
 
+
+//have the gains be the peranent variables up here
 
 /** @brief   Task which 
  *  @details This task 
  *  @param   .
  */
 
-int32_t task_conversion(int16_t x, int16_t y, int32_t enc_feedback_thetam)
+int32_t task_conversion(int16_t coords[2], int32_t enc_feedback_thetam)
 {
-    // create encoder objects
     // create motor object
-    // takes in x and y
+    MotorDriver mot_obj(PA15, D5, D4, D14, D15, PB2);
+
+    // takes in x and y by reading from serial port?
+    /* this isnt right cause 
+     i currently have the x,y array as an input. would it be better
+     to do the serial port reading within this task or outside of it.
+    */
+
     // uses eqn 6 from paper to get the value theta 2
+    int16_t theta_2 = 1+1; // will fill in later
+
     // theta 2 will be plugged into an empiracally found eqn to get theta m
+    int16_t theta_m = theta_2 * 0.5;
+
     // find error signal theta m using encoder feedback
+    int16_t error = theta_m - enc_feedback_thetam;
+
     // convert from thetam to thetam_d by dividing by desired response time
+    int desired_delta_t = 0.5; //seconds; randomly chosen value
+    int16_t thetam_d = theta_m/desired_delta_t;
+
     // give the thetam_d signal to controler
+    int16_t proportional = 
+
     // convert from thetam_d to duty cycle
     // give duty cycle to motor
 
     //IN A SEPARATE TASK THAT I HAVE YET TO MAKE
     // take encoder readings, return motor position, thetam
-    
+
 }
