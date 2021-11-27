ed233468c9d7a3d8810712a67ac9c6b9fcf8e215
mkchandl <mkchandl@calpoly.edu>
2021-11-26 20:59:29 -0800

    task was stuck on creating the second encoder task bc the pins were not on the list of pre-tested ones. Changed to PC6/PC7 tim8 and it fixed the issue. Runs well, will probably need to increase the size of the queues or manage them somehow

--- EncoderTask/EncoderTask.cpp
+++ EncoderTask/EncoderTask.cpp
@@ -30,14 +30,17 @@ void task_encoder(void* p_params)
     Serial.println("starting encoder task");
      
 
-    Queue<uint16_t> data_queue1(500, "enc1_pos");
-    Queue<uint16_t> data_queue2(500, "enc2_pos");
+    Queue<uint16_t> data_queue1(1000, "enc1_pos");
+    Queue<uint16_t> data_queue2(1000, "enc2_pos");
     Serial.println("created shared queues");
 
     // Create encoder timer objects
     STM32Encoder timer_1 (TIM2, PA0, PA1); // timer 2. Arudino pins A0/A1
-    STM32Encoder timer_2 (TIM8, PB0, PA5); // timer 8. Arduino pins A3/D13
-    Serial.println("created enc objects");
+    Serial.println("created enc1 object");
+
+    // STM32Encoder timer_2 (TIM8, PB0, PA5); // timer 8. Arduino pins A3/D13
+    STM32Encoder timer_2 (TIM8, PC6, PC7); // timer 8. Arduino pin D9 and nucleo pin PC6
+    Serial.println("created enc2 object");
     Serial.println("about to enter for loop");
 
     for(;;)
@@ -49,13 +52,13 @@ void task_encoder(void* p_params)
 
         t1_count = timer_1.getCount(); // get new position for delta calc
         t2_count = timer_2.getCount();
-        Serial.print("curr_count:");
-        Serial.println(t2_count);
+        Serial.print("curr_count1:");
+        Serial.println(t1_count);
 
         int16_t delta1 = t1_count - t1_old; // calc delta
         int16_t delta2 = t2_count - t2_old; 
-        Serial.print("delta2:");
-        Serial.println(delta2);
+        Serial.print("delta1:");
+        Serial.println(delta1);
 
         if(abs(delta1)> 0.5*65536) // or .5*32768 not sure which
         {
@@ -94,8 +97,8 @@ void task_encoder(void* p_params)
         t1_pos += delta1;
         t2_pos += delta2;
 
-        Serial.print("t2_pos:");
-        Serial.println(t2_pos);
+        Serial.print("t1_pos:");
+        Serial.println(t1_pos);
 
         data_queue1.put(t1_pos); // read a data point and stuff it into queue
         data_queue2.put(t2_pos);
